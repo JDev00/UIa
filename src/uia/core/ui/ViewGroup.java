3ec37db206a82512ec9d9bcfc7c7a65aa8d53cbb
JDev00 <manuel00.dm@gmail.com>
2023-10-14 21:16:47 +0200

    Adds indexOf method and improves contracts

--- src/uia/core/ui/ViewGroup.java
+++ src/uia/core/ui/ViewGroup.java
@@ -3,19 +3,19 @@ package uia.core.ui;
 /**
  * ViewGroup ADT.
  * <br>
- * ViewGroup is responsible for managing a group of Views.
+ * ViewGroup is designed to manage a set of Views.
  */
 
 public interface ViewGroup extends View, Iterable<View> {
 
     /**
-     * Clip functionality ensures that every graphical component, that lay outside the View's boundaries, isn't drawn.
+     * Clip functionality ensures that every graphical component, that lays outside the View boundaries, isn't drawn.
      * <br>
      * <b>Some notes:</b>
      * <ul>
      *     <li>clip function could be expensive in term of CPU/GPU cycles, so use it only when really needed;</li>
-     *     <li>for every graphical entity, that isn't totally contained inside the View's boundaries, will be
-     *     displayed only the piece that lay inside the View's boundaries.</li>
+     *     <li>for every graphical entity, that isn't totally contained inside the View boundaries, will be
+     *     displayed only the piece that lays inside the View boundaries.</li>
      * </ul>
      *
      * @param clipRegion true to enable clip functionality
@@ -24,13 +24,13 @@ public interface ViewGroup extends View, Iterable<View> {
     void setClip(boolean clipRegion);
 
     /**
-     * @return true it this View has the clip functionality enabled
+     * @return true if this View has the clip functionality enabled
      */
 
     boolean hasClip();
 
     /**
-     * Add the given Views to this group.
+     * Adds the specified Views to this group.
      * <br>
      * A duplicated View won't be added.
      *
@@ -41,7 +41,7 @@ public interface ViewGroup extends View, Iterable<View> {
     void add(View... views);
 
     /**
-     * Add a new View to this group.
+     * Adds a new View to this group
      *
      * @param i    the position where to insert the View
      * @param view a new not null {@link View}
@@ -51,7 +51,7 @@ public interface ViewGroup extends View, Iterable<View> {
     void add(int i, View view);
 
     /**
-     * Remove the given View from this group.
+     * Remove the specified View from this group
      *
      * @param view a not null {@link View} to remove
      */
@@ -59,7 +59,7 @@ public interface ViewGroup extends View, Iterable<View> {
     void remove(View view);
 
     /**
-     * Remove all Views from this group.
+     * Removes all Views from this group
      */
 
     void removeAll();
@@ -71,7 +71,7 @@ public interface ViewGroup extends View, Iterable<View> {
     int size();
 
     /**
-     * @param i the View's position inside this group
+     * @param i the View position inside this group
      * @return the specified View
      * @throws IndexOutOfBoundsException if {@code i < 0 or i >= size()}
      */
@@ -86,7 +86,16 @@ public interface ViewGroup extends View, Iterable<View> {
     View get(String id);
 
     /**
-     * Return the boundaries occupied by the group's Views.
+     * Returns the position of the specified View inside this group
+     *
+     * @param view a {@link View}
+     * @return the view position inside this group otherwise -1
+     */
+
+    int indexOf(View view);
+
+    /**
+     * Returns the boundaries occupied by the group's Views.
      *
      * @return a new array made up of four elements:
      * <ul>
