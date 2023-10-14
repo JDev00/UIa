c42d7e16575b1d8b8318b44bcb78b47c1e415559
JDev00 <manuel00.dm@gmail.com>
2023-10-14 22:19:26 +0200

    Fixes bug in text rotation

--- src/uia/physical/text/InlineRendererV1.java
+++ src/uia/physical/text/InlineRendererV1.java
@@ -11,27 +11,25 @@ import uia.core.ui.ViewText;
 
 public class InlineRendererV1 implements TextRenderer {
 
+    // TODO: change text rotation. Bug.
+
     @Override
     public float draw(ViewText view, Graphic graphic, String text, float x, float y, float rotation) {
         Font font = view.getFont();
 
         char[] chars = text.toCharArray();
 
-        float[] bounds = view.bounds();
-
-        float lineWidth = bounds[2];
+        float lineWidth = view.getWidth();
         float lineHeight = font.getLineHeight();
         float longestLine = font.getWidth(0, chars.length, chars);
 
         float xDist = TextRenderer.map(view.getAlignX()) * (lineWidth - longestLine) / 2f;
         float yDist = TextRenderer.map(view.getAlignY()) * (view.getHeight() - 0.75f * lineHeight) / 2f + 0.75f * lineHeight;
-        float rot = bounds[4];
+        float rot = view.bounds()[4];
 
         graphic.drawText(chars, 0, chars.length,
                 View.getPositionOnX(x, 0f, xDist, yDist, rot),
                 View.getPositionOnY(y, 0f, xDist, yDist, rot),
-                //x + rotX(off_x, off_y, cos(rot), sin(rot)),
-                //y + rotY(off_x, off_y, cos(rot), sin(rot)),
                 rotation);
 
         return longestLine;
--- src/uia/physical/text/MultilineRendererV1.java
+++ src/uia/physical/text/MultilineRendererV1.java
@@ -5,22 +5,15 @@ import uia.core.ui.Graphic;
 import uia.core.ui.View;
 import uia.core.ui.ViewText;
 
-import static uia.utility.TrigTable.*;
-import static uia.utility.TrigTable.sin;
-
 /**
  * Multi line text renderer
  */
 
 public class MultilineRendererV1 implements TextRenderer {
 
-    // TODO: improve performance
-    // TODO: improve code
     @Override
     public float draw(ViewText view, Graphic graphic, String text, float x, float y, float rotation) {
         Font font = view.getFont();
-
-        float[] bounds = view.bounds();
 
         char[] chars = text.toCharArray();
         int length = chars.length;
@@ -30,13 +23,12 @@ public class MultilineRendererV1 implements TextRenderer {
         float longestLine = 0f;
         float textHeight = ViewText.countLines(text) * lineHeight;
 
+        float y_adj = TextRenderer.map(view.getAlignY()) * (view.getHeight() - textHeight - 0.75f * lineHeight) / 2f;
+        float rot = view.bounds()[4];
 
         int sol; // start of line
         int eol = -1; // end of line
         int lines = 0;
-
-        float y_adj = TextRenderer.map(view.getAlignY()) * (view.getHeight() - textHeight - 0.75f * lineHeight) / 2f;
-        float rot = bounds[4];
 
         for (int i = 0; i <= length; i++) {
 
@@ -46,17 +38,13 @@ public class MultilineRendererV1 implements TextRenderer {
 
                 float lineLength = font.getWidth(sol, eol - sol, chars);
 
-                //if (y + (lines + 2) * lineHeight >= top && y + (lines - 1) * lineHeight <= bot) {
-                float off_x = TextRenderer.map(view.getAlignX()) * (lineWidth - lineLength) / 2f;
-                float off_y = (lines + 0.75f) * lineHeight + y_adj;
-                float xDist = rotX(off_x, off_y, cos(rot), sin(rot));
-                float yDist = rotY(off_x, off_y, cos(rot), sin(rot));
+                float xDist = TextRenderer.map(view.getAlignX()) * (lineWidth - lineLength) / 2f;
+                float yDist = (lines + 0.75f) * lineHeight + y_adj;
 
                 graphic.drawText(chars, sol, eol - sol,
                         View.getPositionOnX(x, 0f, xDist, yDist, rot),
                         View.getPositionOnY(y, 0f, xDist, yDist, rot),
                         rotation);
-                //}
 
                 if (lineLength > longestLine) longestLine = lineLength;
 
