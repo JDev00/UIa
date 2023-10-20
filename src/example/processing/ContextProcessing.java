6c0158c96363d026c4bc20c97cabf5f620526118
JDev00 <manuel00.dm@gmail.com>
2023-10-20 22:40:45 +0200

    Moves processing into examples folder

--- src/example/processing/ContextProcessing.java
+++ src/example/processing/ContextProcessing.java
@@ -1,4 +1,4 @@
-package test.processing;
+package example.processing;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
--- src/example/processing/GraphicProcessing.java
+++ src/example/processing/GraphicProcessing.java
@@ -1,4 +1,4 @@
-package test.processing;
+package example.processing;
 
 import processing.core.*;
 
