8fa305191c661b67cf59990f9ab9dba6d3e293a2
JDev00 <manuel00.dm@gmail.com>
2023-10-20 20:15:48 +0200

    Adds examples of usage of basement components and widgets

--- src/test/testSanity.java
+++ src/test/testSanity.java
@@ -1,66 +1,116 @@
 package test;
 
 import test.artefacts.ComponentTracker;
+import uia.application.UIButtonFilled;
+import uia.application.UIButtonList;
+import uia.application.UIButtonSwitch;
+import uia.application.UICalendar;
 import uia.application.awt.ContextAWT;
-import uia.core.Image;
 import uia.core.ui.Context;
-import uia.core.ui.View;
+import uia.core.ui.ViewGroup;
 import uia.core.ui.ViewText;
-import uia.physical.theme.Theme;
-import uia.physical.ComponentText;
 import uia.physical.Component;
 import uia.physical.ComponentGroup;
 import uia.physical.ComponentImage;
-import uia.physical.wrapper.WrapperViewGroup;
+import uia.physical.ComponentText;
+import uia.physical.theme.Theme;
 import uia.utility.Utility;
 
-public class testSanity extends WrapperViewGroup {
+public class testSanity {
 
-    public testSanity(Context context) {
-        super(new ComponentGroup(new Component("TEST_SANITY", 0.5f, 0.5f, 1f, 1f).setExpanseLimit(1f, 1f)));
+    public static ViewText createViewText(String id, float x, float y, float width, float height) {
+        return new ComponentText(
+                new Component(id, x, y, width, height).setExpanseLimit(1f, 1f)
+        );
+    }
 
-        getPaint().setColor(Theme.BACKGROUND);
+    public static ViewGroup createViewGroup(String id, float x, float y, float width, float height) {
+        return new ComponentGroup(
+                new Component(id, x, y, width, height).setExpanseLimit(1f, 1f)
+        );
+    }
 
+    public static ViewGroup createRoot() {
+        ViewGroup result = createViewGroup("ROOT_GROUP", 0.5f, 0.5f, 1f, 1f);
+        result.getPaint().setColor(Theme.BACKGROUND);
+        return result;
+    }
 
-        ViewText text = new ComponentText(new Component("TEXT", 0.33f, 0.25f, 0.5f, 0.75f));
+    public static ViewText createTracker(Context context) {
+        return new ComponentTracker(context, 0.95f, 0.95f, 0.1f, 0.1f);
+    }
+
+    public static Context createAWTContext() {
+        int[] dim = ContextAWT.getScreenSize();
+        Context context = new ContextAWT(4 * dim[0] / 5, 4 * dim[1] / 5);
+        context.start();
+        return context;
+    }
+
+    public static void showBaseComponents() {
+        ViewGroup root = createRoot();
+
+        Context context = createAWTContext();
+        context.setView(root);
+
+        ViewText text = createViewText("TEXT", 0.33f, 0.25f, 0.5f, 0.75f);
         text.setAlign(ViewText.AlignX.RIGHT);
         text.setText(Utility.readAll("src\\test\\testSanity.java"));
         text.setRotation(0.3f);
-
+        text.setSingleLine(true);
 
         ComponentImage image = new ComponentImage(new Component("IMAGE", 0.7f, 0.5f, 0.33f, 0.5f));
         image.getImage().load("sample\\img0.png");
 
-
-        ComponentGroup group = new ComponentGroup(new Component("GROUP", 0.4f, 0.5f, 0.5f, 0.5f)
-                .setExpanseLimit(1.005f, 1.005f));
+        ViewGroup group = createViewGroup("GROUP", 0.4f, 0.5f, 0.5f, 0.5f);
         group.getPaint().setColor(Theme.DARK_GREY);
         group.add(text, image);
 
+        ViewText tracker = createTracker(context);
 
-        ComponentTracker tracker = new ComponentTracker(context, 0.05f, 0.05f, 0.1f, 0.1f);
-
-
-        add(group, tracker);
+        root.add(group, tracker);
     }
 
-    @Override
-    public void update(View parent) {
-        super.update(parent);
+    public static void showWidgets() {
+        ViewGroup root = createRoot();
 
-        try {
-            View view = get("GROUP");
-            view.setRotation(view.bounds()[4] + 0.005f);
-        } catch (Exception ignored) {
-        }
+        Context context = createAWTContext();
+        context.setView(root);
+
+        UIButtonFilled buttonFilled = new UIButtonFilled(
+                new Component("BUTTON_0", 0.85f, 0.25f, 0.15f, 0.1f), true
+        );
+        buttonFilled.getPaint()
+                .setColor(Theme.TRANSPARENT)
+                .setStrokeColor(Theme.WHITE)
+                .setStrokeWidth(4);
+        buttonFilled.getViewText().getTextPaint()
+                .setColor(Theme.WHITE);
+
+        UIButtonSwitch buttonSwitch = new UIButtonSwitch(
+                new Component("BUTTON_1", 0.85f, 0.5f, 0.15f, 0.1f)
+        );
+
+        UIButtonList buttonList = new UIButtonList(new Component("BUTTON_2", 0.85f, 0.75f, 0.15f, 0.1f));
+        buttonList.setText("1\n1", "2", "3", "4", "5");
+
+        /*UIBar bar = new UIBar(new Component("BAR", 0.25f, 0.15f, 0.15f, 0.01f));
+        bar.setRange(0, 1000);
+        bar.setValue(0f);
+        bar.setText(s -> Utility.limitDecimals(s, 1) + " %");
+        bar.rotate(TrigTable.PI);
+        bar.getViewText().getFont().setSize(30f).setStyle(Font.STYLE.BOLD);
+        bar.addCallback((OnClick) pointers -> bar.rotate(bar.geometryBounds()[2] + TrigTable.HALF_PI));*/
+
+        UICalendar calendar = new UICalendar(new Component("CALENDAR", 0.15f, 0.7f, 0.25f, 0.5f));
+        calendar.setRotation(0.05f);
+
+        ViewText tracker = createTracker(context);
+
+        root.add(buttonFilled, buttonSwitch, buttonList, calendar, tracker);
     }
-
-    //
 
     public static void main(String[] args) {
-        int[] dim = ContextAWT.getScreenSize();
-        Context context = new ContextAWT(4 * dim[0] / 5, 4 * dim[1] / 5);
-        context.start();
-        context.setView(new testSanity(context));
+        showBaseComponents();
     }
 }
