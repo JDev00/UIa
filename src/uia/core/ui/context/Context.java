36a756636d8c3e7c4504bc2d2d6720e3230ae045
JDev00 <manuel00.dm@gmail.com>
2023-10-27 20:55:21 +0200

    Refactor: renames methods and objects. Introduces the concept of Context lifecycle

--- src/uia/core/ui/context/Context.java
+++ src/uia/core/ui/context/Context.java
@@ -3,65 +3,79 @@ package uia.core.ui.context;
 import uia.core.ui.View;
 
 /**
- * Context is the base of the UIa framework. It is responsible for updating and rendering a single {@link View} on a {@link Window}.
- * It is intended to be a versatile layer whose implementation is platform dependent.
+ * Context is the base of the UIa framework. It is responsible for managing a single {@link View} displayed on
+ * a {@link Window}. It is intended to be a versatile layer whose implementation is platform dependent.
+ * <br>
+ * <br>
+ * <b>Lifecycle</b>
+ * <p>
+ * As of every application, Context has its own lifecycle. The key idea is that the single lifecycle stage can be set
+ * by the programmer. There are no particular constraints on how to switch between lifecycle stages but one rule
+ * has to be followed: when the Context is created it is on the {@link LifecycleStage#STOP} stage. When the Window
+ * is set to be visible and the rendering process starts, Context will be on the {@link LifecycleStage#RUN} stage.
+ * When the Context is definitively stopped, it will be on the {@link LifecycleStage#TERMINATE} stage.
+ * </p>
+ * <br>
+ * <b>Input emulation</b>
+ * <p>
+ * Sometimes could be useful to emulate some events (for example press a key or move mouse around) to test some assumptions
+ * about event handling and so on without generate the same events manually. For this reason, Context expose the {@link InputEmulator}
+ * class that has the responsibility to emulate the standard inputs based on mouse and keyboard.
+ * </p>
  */
 
 public interface Context {
 
     /**
-     * HINTS are used to change rendering settings
+     * RenderingHint defines the rendering settings
      */
 
-    enum HINT {
+    enum RenderingHint {
         ANTIALIASING_ON, ANTIALIASING_OFF,
-        ANTIALIASING_TEXT_ON, ANTIALIASING_TEXT_OFF,
-        RENDER_QUALITY_COLOR_HIGH, RENDER_QUALITY_COLOR_LOW
+        TEXT_ANTIALIASING_ON, TEXT_ANTIALIASING_OFF,
+        COLOR_QUALITY_HIGH, COLOR_QUALITY_LOW
     }
 
     /**
-     * Clipboard operations
+     * ClipboardOperation defines the operations allowed on system clipboard
      */
 
-    enum CLIPBOARD_OPERATION {COPY, PASTE}
+    enum ClipboardOperation {COPY, PASTE}
 
     /**
-     * Start this Context and make the managed Window visible. If the Context was previously stopped, then
-     * resume the execution.
+     * Lifecycle Context stage
      */
 
-    void start();
+    enum LifecycleStage {RUN, STOP, TERMINATE}
 
     /**
-     * @return true if this Context is currently running
+     * Set the Context lifecycle stage
+     *
+     * @param lifecycleStage a not null {@link LifecycleStage}
+     * @throws NullPointerException if {@code lifecycleStage == null}
      */
 
-    boolean isRunning();
+    void setLifecycleStage(LifecycleStage lifecycleStage);
 
     /**
-     * Stop this Context, which causes the View to stop refreshing and rendering
+     * @return the current {@link LifecycleStage}
      */
 
-    void stop();
-
-    /**
-     * Stop and kill the Context process
-     */
-
-    void kill();
+    LifecycleStage getLifecycleStage();
 
     /**
      * Set the given rendering hints
      *
-     * @param hint a not null array of {@link HINT}s
+     * @param hint a not null array of {@link RenderingHint}s
+     * @throws NullPointerException if {@code hint == null}
      */
 
-    void setHints(HINT... hint);
+    void setRenderingHint(RenderingHint... hint);
 
     /**
-     * Attach a View to this Context.
+     * Let this Context managing the given View.
      * <br>
-     * The given View will be updated and rendered by this Context.
+     * More formally, it updates, dispatches messages/inputs and renders the given View.
      *
      * @param view a {@link View}; it could be null
      */
@@ -69,10 +83,14 @@ public interface Context {
     void setView(View view);
 
     /**
-     * @return the current frame rendering rate (frame rate per second)
+     * Copy a String in the clipboard or paste a String from the clipboard
+     *
+     * @param operation a not null {@link ClipboardOperation}
+     * @param str       a String to copy in the clipboard; it could be null
+     * @return null or the clipboard content
      */
 
-    int getFrameRate();
+    String clipboard(ClipboardOperation operation, String str);
 
     /**
      * @return the {@link Window} managed by this Context
@@ -89,14 +107,4 @@ public interface Context {
      */
 
     InputEmulator getInputEmulator();
-
-    /**
-     * Copy a String in the clipboard or paste a String from the clipboard
-     *
-     * @param operation a not null {@link CLIPBOARD_OPERATION}
-     * @param str       a String to copy in the clipboard; it could be null
-     * @return null or the clipboard content
-     */
-
-    String clipboard(CLIPBOARD_OPERATION operation, String str);
 }
