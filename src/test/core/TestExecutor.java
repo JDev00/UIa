cb78f10ccb9a1a1f0629c68dbf5690eadaac8203
Manu <manuel00.dm@gmail.com>
2024-01-05 21:25:50 +0100

    Improves TestExecutor

--- src/test/core/TestExecutor.java
+++ src/test/core/TestExecutor.java
@@ -9,6 +9,26 @@ import java.lang.reflect.Method;
 public class TestExecutor {
 
     /**
+     * Helper function. Extracts the method marked with {@link BeforeEachTest}
+     *
+     * @param methods a not null array of class methods
+     * @return the marked method or, if it does not exist, null
+     */
+
+    private static Method getBeforeEachTest(Method[] methods) {
+        Method result = null;
+        int i = 0;
+        while (i < methods.length && result == null) {
+            Method currentMethod = methods[i];
+            if (currentMethod.isAnnotationPresent(BeforeEachTest.class)) {
+                result = currentMethod;
+            }
+            i++;
+        }
+        return result;
+    }
+
+    /**
      * Executes the specified test suite
      *
      * @param testSuite a not null test suite to execute
@@ -19,8 +39,20 @@ public class TestExecutor {
 
         new Thread(() -> {
             Method[] methods = testSuite.getClass().getMethods();
+
+            Method beforeEachMethod = getBeforeEachTest(methods);
+
             for (Method method : methods) {
                 if (method.isAnnotationPresent(Test.class) && !method.isAnnotationPresent(Skip.class)) {
+
+                    if (beforeEachMethod != null) {
+                        try {
+                            beforeEachMethod.invoke(testSuite);
+                        } catch (Exception ignored) {
+                            System.out.println("Method marked with @BeforeEachTest can't take arguments");
+                        }
+                    }
+
                     TestAssertion testAssertion = new TestAssertion();
                     try {
                         method.invoke(testSuite, testAssertion);
