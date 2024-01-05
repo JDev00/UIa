53f68948b641bc30f73190223382ad99cae20344
Manu <manuel00.dm@gmail.com>
2024-01-05 21:13:25 +0100

    Improves documentation

--- src/test/core/Skip.java
+++ src/test/core/Skip.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Skip is used to skip a unit test.
+ *
+ * @since 1.3.0
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Skip {
--- src/test/core/Test.java
+++ src/test/core/Test.java
@@ -2,6 +2,13 @@ package test.core;
 
 import java.lang.annotation.*;
 
+/**
+ * Test is used to mark a class method as a unit test. In this way, the testing framework can recognize
+ * and execute the unit test.
+ *
+ * @since 1.3.0
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Test {
