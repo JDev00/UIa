10001e04eb29adc62c01e61a2f8a24cb736cf957
Manu <manuel00.dm@gmail.com>
2024-01-06 11:26:32 +0100

    Improves documentation. Checks if the expected object contains some specified values.

--- src/test/core/TestAssertion.java
+++ src/test/core/TestAssertion.java
@@ -1,19 +1,21 @@
 package test.core;
 
+import java.lang.reflect.Array;
+import java.util.Objects;
+
 /**
- * TestAssertion has the responsibility to check if a specified Object is equal to another one.
- * In addition, it registers all the passed checks.
+ * TestAssertion is responsible for checking whether a specified expected object has the expected properties.
  */
 
 public class TestAssertion {
-    private Object subject;
+    private Object expected;
     private int passedAssertions = 0;
     private int expectedAssertions = 1;
 
     /**
-     * Set the expected assertions to be true
+     * Sets the expected number of true assertions
      *
-     * @param assertions the number of assertions to pass to consider this test passed
+     * @param assertions the number of true assertions
      * @return this TestAssertion
      */
 
@@ -23,33 +25,62 @@ public class TestAssertion {
     }
 
     /**
-     * Set an Object to control
+     * Sets the expected object
      *
-     * @param subject the Object to control; it could be null
+     * @param expected the expected object; it could be null
      * @return this TestAssertion
      */
 
-    public TestAssertion expect(Object subject) {
-        this.subject = subject;
+    public TestAssertion expect(Object expected) {
+        this.expected = expected;
         return this;
     }
 
     /**
-     * Check that the subject is equal to the specified value
+     * Checks if the expected object is equal to the specified value
      *
-     * @throws RuntimeException if {@code subject != value}
+     * @throws IllegalArgumentException if {@code expected != value}
      */
 
     public void toBe(Object value) {
-        if ((subject == null && value != null) || !subject.equals(value)) {
-            throw new RuntimeException("expected " + subject + " to be " + value + " but it wasn't");
+        if ((expected == null && value != null) || !expected.equals(value)) {
+            throw new IllegalArgumentException("expected " + expected + " to be " + value + " but it wasn't");
         } else {
             passedAssertions++;
         }
     }
 
     /**
-     * @return true if this TestAssertions passed all assertions
+     * Checks whether the expected object contains the specified values.
+     * <br>
+     * A shallow control is operated on elements.
+     *
+     * @param values the values the expected object is supposed to contain
+     * @throws IllegalArgumentException if {@code values.length > expected.length or values[i] != expected[i]}
+     * @throws ClassCastException       if {@code expected is not an array}
+     * @throws NullPointerException     if {@code values is null}
+     */
+
+    public void toHaveValues(Object... values) {
+        Objects.requireNonNull(values);
+        if (expected.getClass().isArray()) {
+            if (values.length > Array.getLength(expected)) {
+                throw new IllegalArgumentException();
+            }
+            for (int i = 0; i < values.length; i++) {
+                Object subjectElement = Array.get(expected, i);
+                if (!subjectElement.equals(values[i])) {
+                    throw new IllegalArgumentException("expected " + subjectElement + " to be " + values[i] + " but it wasn't");
+                }
+            }
+            passedAssertions++;
+        } else {
+            throw new ClassCastException("expected " + expected + " to be an array but it wasn't");
+        }
+    }
+
+    /**
+     * @return true if this TestAssertions has passed all the required assertions
      */
 
     public boolean passed() {
