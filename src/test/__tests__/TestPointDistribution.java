afb368b9e3114f4ff0f5fc26b6045648f82cf9a8
Manu <manuel00.dm@gmail.com>
2024-01-06 15:17:11 +0100

    Updates pointDistribution tests

--- src/test/__tests__/TestPointDistribution.java
+++ src/test/__tests__/TestPointDistribution.java
@@ -1,5 +1,6 @@
 package test.__tests__;
 
+import test.core.BeforeEachTest;
 import test.core.TestExecutor;
 import uia.application.math.PointDistribution;
 import test.core.Test;
@@ -10,9 +11,12 @@ import test.core.TestAssertion;
  */
 
 public class TestPointDistribution {
-    static PointDistribution pointDistribution = new PointDistribution();
+    PointDistribution pointDistribution;
 
-    static {
+    @BeforeEachTest
+    public void beforeEach() {
+        pointDistribution = new PointDistribution();
+
         float[][] distribution = {
                 new float[]{0, 0},
                 new float[]{-4999.99f, 0},
@@ -21,38 +25,53 @@ public class TestPointDistribution {
                 new float[]{-4566, 677},
                 new float[]{0.1f, 0.0001f}
         };
-
         for (float[] point : distribution) {
             pointDistribution.add(point[0], point[1]);
         }
     }
 
     @Test
-    public static void minimumPointShouldBeObtained(TestAssertion testAssertion) {
-        testAssertion.assertions(2);
-        testAssertion.expect(pointDistribution.getMin(PointDistribution.AXIS.X)).toBe(-4999.99f);
-        testAssertion.expect(pointDistribution.getMin(PointDistribution.AXIS.Y)).toBe(-5000f);
+    public void theMinimumPointShouldBeObtainedFromPointDistribution(TestAssertion testAssertion) {
+        testAssertion.assertions(1);
+
+        float[] expectedMinimumPoint = {
+                pointDistribution.getMin(PointDistribution.AXIS.X),
+                pointDistribution.getMin(PointDistribution.AXIS.Y)
+        };
+        testAssertion.expect(expectedMinimumPoint).toHaveValues(-4999.99f, -5000f);
     }
 
     @Test
-    public static void maximumPointShouldBeObtained(TestAssertion testAssertion) {
-        testAssertion.assertions(2);
-        testAssertion.expect(pointDistribution.getMax(PointDistribution.AXIS.X)).toBe(2345f);
-        testAssertion.expect(pointDistribution.getMax(PointDistribution.AXIS.Y)).toBe(4998.98f);
+    public void theMaximumPointShouldBeObtainedFromPointDistribution(TestAssertion testAssertion) {
+        testAssertion.assertions(1);
+
+        float[] expectedMaximumPoint = {
+                pointDistribution.getMax(PointDistribution.AXIS.X),
+                pointDistribution.getMax(PointDistribution.AXIS.Y)
+        };
+        testAssertion.expect(expectedMaximumPoint).toHaveValues(2345f, 4998.98f);
     }
 
     @Test
-    public static void meanShouldBeObtained(TestAssertion testAssertion) {
-        testAssertion.assertions(2);
-        testAssertion.expect(pointDistribution.getMean(PointDistribution.AXIS.X)).toBe(-1195.14833f);
-        testAssertion.expect(pointDistribution.getMean(PointDistribution.AXIS.Y)).toBe(112.66335f);
+    public void theMeanShouldBeObtainedFromPointDistribution(TestAssertion testAssertion) {
+        testAssertion.assertions(1);
+
+        float[] expectedMean = {
+                pointDistribution.getMean(PointDistribution.AXIS.X),
+                pointDistribution.getMean(PointDistribution.AXIS.Y)
+        };
+        testAssertion.expect(expectedMean).toHaveValues(-1195.14833f, 112.66335f);
     }
 
     @Test
-    public static void standardDeviationShouldBeObtained(TestAssertion testAssertion) {
-        testAssertion.assertions(2);
-        testAssertion.expect(pointDistribution.getStandardDeviation(PointDistribution.AXIS.X)).toBe(2472.1162f);
-        testAssertion.expect(pointDistribution.getStandardDeviation(PointDistribution.AXIS.Y)).toBe(2682.5352f);
+    public void theStandardDeviationShouldBeObtainedFromPointDistribution(TestAssertion testAssertion) {
+        testAssertion.assertions(1);
+
+        float[] expectedStandardDeviation = {
+                pointDistribution.getStandardDeviation(PointDistribution.AXIS.X),
+                pointDistribution.getStandardDeviation(PointDistribution.AXIS.Y)
+        };
+        testAssertion.expect(expectedStandardDeviation).toHaveValues(2472.1162f, 2682.5352f);
     }
 
     public static void main(String[] args) {
