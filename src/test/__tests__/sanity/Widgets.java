0efe50a222e1a732f89a7ce47e535010423dde69
Manu <manuel00.dm@gmail.com>
2024-03-12 23:45:24 +0100

    Refactor: renames file and extracts method

---
+++ src/test/__tests__/sanity/Widgets.java
@@ -0,0 +1,60 @@
+package test.__tests__.sanity;
+
+import uia.application.UIButtonFilled;
+import uia.application.UIButtonList;
+import uia.application.UIToggleButton;
+import uia.application.UICalendar;
+import uia.core.ui.context.Context;
+import uia.core.ui.ViewGroup;
+import uia.physical.Component;
+import uia.physical.theme.Theme;
+
+import static test.__tests__.utility.TestUtility.*;
+
+/**
+ * Sanity test.
+ */
+
+public class Widgets {
+
+    /**
+     * Builds some widgets component.
+     */
+
+    public static ViewGroup buildWidgets() {
+        UIButtonFilled buttonFilled = new UIButtonFilled(
+                new Component("BUTTON_0", 0.85f, 0.25f, 0.15f, 0.1f), true
+        );
+        buttonFilled.getPaint()
+                .setColor(Theme.TRANSPARENT)
+                .setStrokeColor(Theme.WHITE)
+                .setStrokeWidth(4);
+        buttonFilled.getTextPaint()
+                .setColor(Theme.WHITE);
+
+        UIToggleButton buttonSwitch = new UIToggleButton(
+                new Component("BUTTON_1", 0.85f, 0.5f, 0.15f, 0.1f)
+        );
+
+        UIButtonList buttonList = new UIButtonList(
+                new Component("BUTTON_2", 0.85f, 0.75f, 0.15f, 0.1f)
+        );
+        buttonList.setText("1\n1", "2", "3", "4", "5");
+
+        UICalendar calendar = new UICalendar(
+                new Component("CALENDAR", 0.15f, 0.7f, 0.25f, 0.5f)
+        );
+        calendar.setRotation(0.05f);
+
+        ViewGroup result = createRoot();
+        ViewGroup.insert(result, buttonFilled, buttonSwitch, buttonList, calendar);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ViewGroup root = buildWidgets();
+
+        Context context = createMockContext();
+        context.setView(root);
+    }
+}
---
+++ src/test/__tests__/sanity/standardComponents.java
@@ -0,0 +1,54 @@
+package test.__tests__.sanity;
+
+import test.__tests__.utility.TestUtility;
+import uia.core.ui.ViewGroup;
+import uia.core.ui.ViewText;
+import uia.core.ui.callbacks.OnClick;
+import uia.core.ui.context.Context;
+import uia.physical.Component;
+import uia.physical.ComponentImage;
+import uia.physical.theme.Theme;
+
+import static test.__tests__.utility.TestUtility.*;
+
+/**
+ * Sanity test.
+ */
+
+public class standardComponents {
+
+    /**
+     * Shows the base components
+     */
+
+    public static ViewGroup buildBasementsComponents() {
+        ViewText text = createViewText("TEXT", 0.33f, 0.45f, 0.5f, 0.75f);
+        text.setAlign(ViewText.AlignX.RIGHT);
+        text.setAlign(ViewText.AlignY.CENTER);
+        text.setText("Hello world!");
+        text.setRotation(0.3f);
+
+        ComponentImage image = new ComponentImage(
+                new Component("IMAGE", 0.7f, 0.5f, 0.33f, 0.5f)
+        );
+        image.getImage().load("sample\\img0.png");
+        image.registerCallback((OnClick) touches -> System.out.println("ComponentImage clicked!"));
+
+        ViewGroup group = createViewGroup("GROUP", 0.4f, 0.5f, 0.5f, 0.5f);
+        group.getPaint().setColor(Theme.LIGHT_GRAY);
+        group.setRotation(0.1f);
+
+        ViewGroup.insert(group, image, text);
+
+        ViewGroup result = createRoot();
+        ViewGroup.insert(result, group);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ViewGroup group = buildBasementsComponents();
+
+        Context context = TestUtility.createMockContext();
+        context.setView(group);
+    }
+}
