b9e281e37944d63eeb0f2a4a9b30ca8059c32d0e
Manu <manuel00.dm@gmail.com>
2024-03-17 15:23:52 +0100

    Updates documentation

--- src/uia/application/UICalendar.java
+++ src/uia/application/UICalendar.java
@@ -21,16 +21,11 @@ import uia.utility.CalendarUtility;
 import uia.utility.MathUtility;
 import uia.utility.Geometries;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 
 /**
- * UI Gregorian Calendar
+ * UIa built-in gregorian calendar.
  */
-
-// TODO: complete calendar
 
 public final class UICalendar extends WrapperView {
     public static final String[] WEEK = new String[]{"M", "T", "W", "T", "F", "S", "S"};
@@ -40,7 +35,7 @@ public final class UICalendar extends WrapperView {
     };
 
     private final Calendar calendar;
-    private final UIButtonList listUI;
+    private final UIButtonList header;
     private final Cell[] cells = new Cell[38];
     private final Font font;
     private final Paint[] paintCell = {
@@ -51,23 +46,28 @@ public final class UICalendar extends WrapperView {
 
     private int days;
     private int offset;
-    private int sDay;
-    private int sMonth;
-    private int sYear;
+    private final int[] currentDate = {1, 1, 2024};
 
     public UICalendar(View view) {
         super(new ComponentGroup(view));
-
         setGeometry(g -> Drawable.buildRect(g, getWidth(), getHeight(), Geometries.STD_ROUND), true);
         getPaint().setColor(ThemeDarcula.DARK_GRAY);
 
-        font = new Font("Arial", Font.STYLE.ITALIC, Font.FONT_SIZE_DESKTOP);
+        calendar = GregorianCalendar.getInstance();
 
+        font = Font.createDesktopFont(Font.Style.ITALIC);
 
-        listUI = (UIButtonList) createHeader(font);
-        listUI.getViewRight().registerCallback((OnClick) touches -> setDate(sDay, sMonth + 1, sYear));
-        listUI.getViewLeft().registerCallback((OnClick) touches -> setDate(sDay, sMonth - 1, sYear));
-
+        header = createHeader(font);
+        header.getViewRight().registerCallback((OnClick) touches -> setDate(
+                currentDate[0],
+                currentDate[1] + 1,
+                currentDate[2])
+        );
+        header.getViewLeft().registerCallback((OnClick) touches -> setDate(
+                currentDate[0],
+                currentDate[1] - 1,
+                currentDate[2])
+        );
 
         for (int i = 0; i < 7; i++) {
             cells[i] = Cell.createWeekDay(WEEK[i]);
@@ -82,7 +82,8 @@ public final class UICalendar extends WrapperView {
                     paint.set(paintCell[1]);
                 } else {
                     paint.set(paintCell[2]);
-                    notifyCallbacks(OnSelect.class, getDate()[0]);
+                    int day = getDate()[0];
+                    notifyCallbacks(OnSelect.class, day);
                 }
             });
             cell.registerCallback((OnMouseEnter) touches -> {
@@ -101,17 +102,16 @@ public final class UICalendar extends WrapperView {
             i.getPaint().set(paintCell[0]);
         }
 
-        calendar = GregorianCalendar.getInstance();
+        int[] currentDate = CalendarUtility.getDate();
+        setDate(currentDate[0], currentDate[1], currentDate[2]);
 
-        int[] date = CalendarUtility.getDate();
-        setDate(date[0], date[1], date[2]);
-
-        ViewGroup.insert(getView(), listUI);
-        ViewGroup.insert(getView(), cells);
+        ViewGroup group = getView();
+        ViewGroup.insert(group, header);
+        ViewGroup.insert(group, cells);
     }
 
     /**
-     * Event called when a day has been select.
+     * Callback invoked when a day is selected.
      * <br>
      * It provides the selected day.
      */
@@ -120,21 +120,181 @@ public final class UICalendar extends WrapperView {
     }
 
     /**
-     * Event called when a change in calendar's month and/or year is detected.
+     * Callback invoked when a change in calendar month or year is detected.
      * <br>
-     * It provides the new date as an array made of: day, month and year.
+     * It provides the date as an array made of: day, month and year.
      */
 
     public interface OnChange extends Callback<Integer[]> {
     }
 
     /**
-     * Create the calendar's header
+     * Sets the calendar date.
+     *
+     * @param day   the day of the month between [1, 31]
+     * @param month the month between [1, 12]
+     * @param year  the year
      */
 
-    private static View createHeader(Font font) {
-        UIButtonList view = new UIButtonList(new Component("HEADER", 0.5f, 0.15f, 0.9f, 0.2f)
-                .setExpanseLimit(1f, 1f));
+    public void setDate(int day, int month, int year) {
+        if (month > 12) {
+            month = 1;
+            year++;
+        } else if (month < 1) {
+            month = 12;
+            year--;
+        }
+
+        calendar.clear();
+        calendar.set(year, month - 1, 1);
+
+        // update data
+        days = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+        offset = CalendarUtility.getDay(calendar.get(Calendar.DAY_OF_WEEK));
+
+        // update selection
+        int prevCurrentDay = currentDate[0];
+        if (prevCurrentDay > days && cells[7 + prevCurrentDay - 1].selected) {
+            cells[7 + prevCurrentDay - 1].selected = false;
+        }
+
+        // update current date
+        currentDate[0] = MathUtility.constrain(day, 1, days);
+        currentDate[1] = month;
+        currentDate[2] = year;
+
+        // update month
+        header.setText(MONTHS[month - 1] + " " + year);
+
+        notifyCallbacks(OnChange.class, getDate());
+    }
+
+    /**
+     * Sets the name of the week days.
+     * <br>
+     * Note that the first day is monday and the last is sunday.
+     *
+     * @param days the days of the week
+     * @throws NullPointerException if {@code days == null}
+     */
+
+    public void setWeek(String... days) {
+        Objects.requireNonNull(days);
+
+        for (int i = 0; i < days.length; i++) {
+            cells[i].setText(days[i]);
+        }
+    }
+
+    /**
+     * Adjusts the font size according to the View dimension.
+     */
+    private void updateFontSize(float width, float height) {
+        float fontSize = Math.min(
+                Math.min(width * getWidth(), height * getHeight()),
+                Font.DESKTOP_SIZE
+        );
+        if (fontSize != font.getSize()) {
+            font.setSize(fontSize);
+        }
+    }
+
+    @Override
+    public void update(View container) {
+        super.update(container);
+
+        if (isVisible()) {
+            float[] cellDim = {0.7f / 6f, 0.08f};
+
+            float weekCellPosY = 1f / 3f;
+            for (int i = 0; i < 7; i++) {
+                cells[i].setPosition(0.15f + cellDim[0] * i, weekCellPosY);
+            }
+
+            float dayCellPosY = weekCellPosY + cellDim[0];
+            float gap = (0.95f - dayCellPosY) / 5f;
+            for (int i = 0; i < 31; i++) {
+                Cell cell = cells[7 + i];
+                cell.setPosition(
+                        0.15f + cellDim[0] * ((i + offset) % 7),
+                        dayCellPosY + gap * ((i + offset) / 7)
+                );
+                cell.setVisible(i < days);
+            }
+
+            for (Cell cell : cells) {
+                cell.setDimension(cellDim[0], cellDim[1]);
+                cell.update(this);
+            }
+
+            updateFontSize(cellDim[0], cellDim[1]);
+        }
+    }
+
+    /**
+     * @return the calendar text {@link Font}
+     */
+
+    public Font getFont() {
+        return font;
+    }
+
+    /**
+     * @return the {@link Paint} object used to color days
+     */
+
+    public Paint getPaintDay() {
+        return paintCell[0];
+    }
+
+    /**
+     * @return the {@link Paint} object used to color a cursor covered day
+     */
+
+    public Paint getPaintHover() {
+        return paintCell[1];
+    }
+
+    /**
+     * @return the {@link Paint} object used to highlight a selected day
+     */
+
+    public Paint getHighlightPaint() {
+        return paintCell[2];
+    }
+
+    /**
+     * @return the current calendar date, as new array, made of: day, month and year
+     */
+
+    public int[] getDate() {
+        return new int[]{
+                currentDate[0],
+                currentDate[1],
+                currentDate[2]
+        };
+    }
+
+    /**
+     * @return the selected days
+     */
+
+    public List<Integer> getSelectedDays() {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < days; i++) {
+            if (cells[7 + i].selected) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Creates the calendar header view.
+     */
+
+    private static UIButtonList createHeader(Font font) {
+        UIButtonList view = new UIButtonList(new Component("HEADER", 0.5f, 0.15f, 0.9f, 0.2f));
         view.setConsumer(Consumer.SCREEN_TOUCH, false);
         view.getPaint().setColor(Theme.TRANSPARENT);
 
@@ -156,147 +316,7 @@ public final class UICalendar extends WrapperView {
     }
 
     /**
-     * Set the calendar's date
-     *
-     * @param d the day of the month between [1, 31]
-     * @param m the month between [1, 12]
-     * @param y the year
-     */
-
-    public void setDate(int d, int m, int y) {
-        if (m > 12) {
-            m = 1;
-            y++;
-        } else if (m < 1) {
-            m = 12;
-            y--;
-        }
-
-        calendar.clear();
-        calendar.set(y, m - 1, 1);
-
-        sMonth = m;
-        sYear = y;
-        days = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
-
-        // update selection
-        if (sDay > days && cells[7 + sDay - 1].selected) cells[7 + sDay - 1].selected = false;
-
-        sDay = MathUtility.constrain(d, 1, days);
-
-        offset = CalendarUtility.getDay(calendar.get(Calendar.DAY_OF_WEEK));
-
-        listUI.setText(MONTHS[m - 1] + " " + y);
-
-        notifyCallbacks(OnChange.class, getDate());
-    }
-
-    /**
-     * Set the name of the week's days.
-     * <br>
-     * Note that the first day is monday and the last is sunday.
-     *
-     * @param days a not null array
-     */
-
-    public void setWeek(String... days) {
-        for (int i = 0; i < days.length; i++) {
-            cells[i].setText(days[i]);
-        }
-    }
-
-    private void updateFontSize(float width, float height) {
-        float fontSize = Math.min(
-                Math.min(width * getWidth(), height * getHeight()),
-                Font.FONT_SIZE_DESKTOP
-        );
-        if (fontSize != font.getSize()) font.setSize(fontSize);
-    }
-
-    @Override
-    public void update(View container) {
-        super.update(container);
-
-        if (isVisible()) {
-            float dx = 0.7f / 6f;
-            float dy = 0.08f;
-            float y1 = 1f / 3f;
-            float y2 = y1 + dx;
-            float gap = (0.95f - y2) / 5f;
-
-            for (int i = 0; i < 7; i++) {
-                cells[i].setPosition(0.15f + dx * i, y1);
-            }
-
-            for (int i = 0; i < 31; i++) {
-                Cell cell = cells[7 + i];
-                cell.setPosition(0.15f + dx * ((i + offset) % 7), y2 + gap * ((i + offset) / 7));
-                cell.setVisible(i < days);
-            }
-
-            for (Cell i : cells) {
-                i.setDimension(dx, dy);
-                i.update(this);
-            }
-
-            updateFontSize(dx, dy);
-        }
-    }
-
-    /**
-     * @return the calendar's {@link Font}
-     */
-
-    public Font getFont() {
-        return font;
-    }
-
-    /**
-     * @return the {@link Paint} used to color days
-     */
-
-    public Paint getPaintDay() {
-        return paintCell[0];
-    }
-
-    /**
-     * @return the {@link Paint} used to color a cursor covered day
-     */
-
-    public Paint getPaintOver() {
-        return paintCell[1];
-    }
-
-    /**
-     * @return the {@link Paint} used when a day is selected
-     */
-
-    public Paint getPaintSelected() {
-        return paintCell[2];
-    }
-
-    /**
-     * @return the calendar's date as new array made of: day, month and year
-     */
-
-    public int[] getDate() {
-        return new int[]{sDay, sMonth, sYear};
-    }
-
-    /**
-     * @return a new {@link List} filled with the selected days
-     */
-
-    public List<Integer> getSelectedDays() {
-        List<Integer> out = new ArrayList<>();
-        for (int i = 0; i < days; i++) {
-            if (cells[7 + i].selected) out.add(i + 1);
-        }
-        return out;
-    }
-
-    /**
-     * Cell is a general item used to represent a day or a day of week
+     * Cell is a generic item used to represent a day or a week day.
      */
 
     private static class Cell extends WrapperViewText {
@@ -304,15 +324,14 @@ public final class UICalendar extends WrapperView {
 
         public Cell(String id) {
             super(new ComponentText(
-                    new Component("CELL_" + id, 0f, 0f, 0f, 0f))
+                    new Component("CALENDAR_CELL_" + id, 0f, 0f, 0f, 0f))
             );
-
-            setAlign(ViewText.AlignY.CENTER);
             setConsumer(Consumer.SCREEN_TOUCH, false);
+            setAlign(ViewText.AlignY.CENTER);
         }
 
         /**
-         * Create a new Cell used to represent a day of weeks
+         * Creates a new Cell used to represent a week day.
          */
 
         public static Cell createWeekDay(String weekDay) {
@@ -323,7 +342,7 @@ public final class UICalendar extends WrapperView {
         }
 
         /**
-         * Create a new Cell used to represent a day
+         * Creates a new Cell used to represent a day.
          */
 
         public static Cell createDay(String day) {
