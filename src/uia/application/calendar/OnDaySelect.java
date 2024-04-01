8399d2c82d27c2c74c6bb6b48b777207d4aab259
Manu <manuel00.dm@gmail.com>
2024-04-01 16:19:11 +0200

    Updates documentation

--- src/uia/application/calendar/OnDaySelect.java
+++ src/uia/application/calendar/OnDaySelect.java
@@ -3,7 +3,7 @@ package uia.application.calendar;
 import uia.core.basement.Callback;
 
 /**
- * OnDaySelect is a callback that is invoked when a day is selected/deselected on a calendar.
+ * OnDaySelect is triggered when a day is selected/deselected on the calendar.
  * <br>
  * It provides the selected/deselected day.
  */
