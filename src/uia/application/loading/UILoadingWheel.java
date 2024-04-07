7e67a97708d07b72ab122f08915e739d9909cdfb
Manu <manuel00.dm@gmail.com>
2024-04-07 15:09:32 +0200

    Refactor: changes constructor signature

--- src/uia/application/loading/UILoadingWheel.java
+++ src/uia/application/loading/UILoadingWheel.java
@@ -1,10 +1,10 @@
 package uia.application.loading;
 
 import uia.physical.component.WrapperView;
-import uia.physical.component.Component;
 import uia.utility.MathUtility;
 import uia.core.shape.Geometry;
 import uia.utility.Geometries;
+import uia.core.ui.View;
 
 /**
  * Standard UIa component.
@@ -18,8 +18,8 @@ public final class UILoadingWheel extends WrapperView {
 
     private float rotation = 0f;
 
-    public UILoadingWheel(String id, float velocity, float thickness) {
-        super(new Component(id, 0.5f, 0.5f, 1f, 1f));
+    public UILoadingWheel(View view, float velocity, float thickness) {
+        super(view);
         setColliderPolicy(ColliderPolicy.AABB);
         setGeometry(g -> {
             if (Math.abs(rotation) >= MathUtility.TWO_PI) {
