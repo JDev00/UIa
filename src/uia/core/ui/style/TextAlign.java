bee9382995a1b2c3874b507b704922b9735ded72
Manu <manuel00.dm@gmail.com>
2024-06-01 23:06:10 +0200

    Refactor: extracts component

---
+++ src/uia/core/ui/style/TextAlign.java
@@ -0,0 +1,13 @@
+package uia.core.ui.style;
+
+/**
+ * The TextVerticalAlignment collects the vertical alignment for text.
+ */
+
+enum TextVerticalAlignment {TOP, CENTER, BOTTOM}
+
+/**
+ * The TextHorizontalAlignment collects the horizontal alignment for text.
+ */
+
+enum TextHorizontalAlignment {LEFT, CENTER, RIGHT}
