ae5cd3de873cd349a778283e4a60dfa9b2c97bf3
Manu <manuel00.dm@gmail.com>
2024-06-01 22:25:36 +0200

    Changes Graphics API

--- src/uia/core/ui/Graphics.java
+++ src/uia/core/ui/Graphics.java
@@ -1,6 +1,6 @@
 package uia.core.ui;
 
-import uia.core.paint.Paint;
+import uia.core.paint.Color;
 import uia.core.shape.Shape;
 import uia.core.*;
 
@@ -20,7 +20,7 @@ import uia.core.*;
  *     <li>draw an image.</li>
  * </ul>
  * And, for each drawing operation, make it possible to modify the graphical appearance of the object
- * (depending on the drawing operation).
+ * being rendered.
  */
 
 public interface Graphics {
@@ -31,6 +31,73 @@ public interface Graphics {
      */
 
     void dispose();
+
+    // shape
+
+    /**
+     * Sets the width of the line used to display the shape border.
+     * <br>
+     * <i>Every Shape rendered after this method will be affected.</i>
+     *
+     * @param lineWidth the line width greater than or equal to zero
+     * @return this Graphics
+     * @since 1.6.0
+     */
+
+    Graphics setShapeBorderWidth(int lineWidth);
+
+    /**
+     * Sets the shape border color.
+     * <br>
+     * <i>Every Shape rendered after this method will be affected.</i>
+     *
+     * @param color the shape border color
+     * @return this Graphics
+     * @throws NullPointerException if {@code == null}
+     * @since 1.6.0
+     */
+
+    Graphics setShapeBorderColor(Color color);
+
+    /**
+     * Sets a Color object that will be used to paint the shapes rendered after this method is called.
+     * <br>
+     * <i>Every Shape rendered after this method will be affected.</i>
+     *
+     * @param color the Color used to paint shapes
+     * @return this Graphics
+     * @throws NullPointerException if {@code color == null}
+     * @since 1.6.0
+     */
+
+    Graphics setShapeColor(Color color);
+
+    /**
+     * Draws an on-the-fly shape on this Graphics.
+     *
+     * @param vertices an array with the shape vertices. The array shape must be: [x1,y1, x2,y2, x3,y3, ...]
+     * @return this Graphics
+     * @throws NullPointerException     if {@code vertices == null}
+     * @throws IllegalArgumentException if {@code vertices doesn't follow the required array shape}
+     * @apiNote drawShape(float...) is less powerful compared to {@link #drawShape(Shape)}, in fact
+     * vertices don't support translation, scaling and rotation. However, vertices can be expressed in a free form
+     * without value constraints.
+     * @implSpec This method must draw a self-contained shape and must not interfere with the {@link #drawShape(Shape)}.
+     * The first vertex must be used to close the shape.
+     * @since 1.4.0
+     */
+
+    Graphics drawShape(float... vertices);
+
+    /**
+     * Draws the given {@link Shape} on this Graphics.
+     *
+     * @param shape a not null {@link Shape} to be drawn
+     * @return this Graphics
+     * @throws NullPointerException if {@code font == null}
+     */
+
+    Graphics drawShape(Shape shape);
 
     /**
      * Sets the current clipping area to an arbitrary clip shape.
@@ -48,16 +115,7 @@ public interface Graphics {
 
     void restoreClip();
 
-    /**
-     * Sets a Paint used to control the graphic properties of a Shape/text.
-     * <br>
-     * Every piece of Shape/text rendered after {@code setPaint(Paint)} will be affected.
-     *
-     * @param paint a not null {@link Paint}
-     * @throws NullPointerException if {@code paint == null}
-     */
-
-    void setPaint(Paint paint);
+    // text
 
     /**
      * Sets the Font used to display text.
@@ -65,35 +123,22 @@ public interface Graphics {
      * Every text rendered after {@code setFont(Font)} will be affected.
      *
      * @param font a not null {@link Font}
+     * @return this Graphics
      * @throws NullPointerException if {@code font == null}
      */
 
-    void setFont(Font font);
+    Graphics setFont(Font font);
 
     /**
-     * Draws an on-the-fly shape on this Graphics.
+     * Sets a Color object that will be used to color text after this method is called.
      *
-     * @param vertices an array with the shape vertices. The array shape must be: [x1,y1, x2,y2, x3,y3, ...]
-     * @throws NullPointerException     if {@code vertices == null}
-     * @throws IllegalArgumentException if {@code vertices doesn't follow the required array shape}
-     * @apiNote drawShape(float...) is less powerful compared to {@link #drawShape(Shape)}, in fact
-     * vertices don't support translation, scaling and rotation. However, vertices can be expressed in a free form
-     * without value constraints.
-     * @implSpec This method must draw a self-contained shape and must not interfere with the {@link #drawShape(Shape)}.
-     * The first vertex must be used to close the shape.
-     * @since 1.4
+     * @param color the Color used to paint text
+     * @return this Graphics
+     * @throws NullPointerException if {@code color == null}
+     * @since 1.6.0
      */
 
-    void drawShape(float... vertices);
-
-    /**
-     * Draws the given {@link Shape} on this Graphics.
-     *
-     * @param shape a not null {@link Shape} to be drawn
-     * @throws NullPointerException if {@code font == null}
-     */
-
-    void drawShape(Shape shape);
+    Graphics setTextColor(Color color);
 
     /**
      * Draws the given character array. The baseline of the first character is at position (<i>x</i>,<i>y</i>).
@@ -104,10 +149,13 @@ public interface Graphics {
      * @param x        the <i>x</i> coordinate of the baseline of the text
      * @param y        the <i>y</i> coordinate of the baseline of the text
      * @param rotation the text's rotation in radians
+     * @return this Graphics
      * @throws NullPointerException if {@code data == null}
      */
 
-    void drawText(char[] data, int offset, int length, float x, float y, float rotation);
+    Graphics drawText(char[] data, int offset, int length, float x, float y, float rotation);
+
+    // image
 
     /**
      * Draws as much of the specified image as has already been scaled to fit inside the specified rectangle.
@@ -120,8 +168,9 @@ public interface Graphics {
      * @param width    the width of the rectangle
      * @param height   the height of the rectangle
      * @param rotation the image's rotation around the pivot
+     * @return this Graphics
      * @throws NullPointerException if {@code image == null}
      */
 
-    void drawImage(Image image, float x, float y, float width, float height, float rotation);
+    Graphics drawImage(Image image, float x, float y, float width, float height, float rotation);
 }
