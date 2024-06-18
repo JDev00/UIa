3cd3e26d4f76c158ca12641287920565797114d0
Manu <manuel00.dm@gmail.com>
2024-06-18 21:03:21 +0200

    Adds Pair data structure

---
+++ src/uia/utility/structure/Pair.java
@@ -0,0 +1,31 @@
+package uia.utility.structure;
+
+/**
+ * Pair is responsible for storing two objects.
+ */
+
+public class Pair<K, V> {
+    private final K key;
+    private final V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
