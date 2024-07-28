0bbd4dc1a91793dd02be57f535813f61e7ea3fb2
Manu <manuel00.dm@gmail.com>
2024-07-28 13:17:58 +0200

    updates test

---
+++ src/test/TestMessagingSystem.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import uia.core.ui.callbacks.OnMessageReceived;
+import uia.application.message.MessageFactory;
+import uia.core.basement.message.Message;
+import uia.core.context.Context;
+import uia.core.ui.ViewGroup;
+import uia.core.ui.View;
+
+import static utility.TestUtility.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * MessagingSystem unit tests.
+ */
+
+class TestMessagingSystem {
+    Context context;
+    ViewGroup rootView;
+
+    @BeforeEach
+    public void beforeEach() {
+        rootView = createRoot();
+
+        context = createMockContext();
+        context.setView(rootView);
+    }
+
+    @Test
+    void allMessagesShouldBeReceivedByTheTargetView() {
+        String MESSAGE_PAYLOAD = "hello";
+        String TARGET_VIEW_ID = "B";
+        int numberOfMessages = 50_000;
+        // counts the number of passed assertions
+        int[] assertionsPassed = {0};
+
+        // setup
+        View targetView = createView(TARGET_VIEW_ID, 0f, 0f, 0.1f, 0.1f);
+        targetView.registerCallback((OnMessageReceived) receivedMessage -> {
+            String messagePayload = receivedMessage.getPayload();
+            // tests the message equivalence
+            if (MESSAGE_PAYLOAD.equals(messagePayload)) {
+                assertionsPassed[0]++;
+            }
+        });
+        ViewGroup.insert(rootView, targetView);
+
+        // act
+        for (int i = 0; i < numberOfMessages; i++) {
+            Message message = MessageFactory.create(MESSAGE_PAYLOAD, TARGET_VIEW_ID);
+            rootView.sendMessage(message);
+        }
+
+        // verify
+        waitFor(2_000);
+        assertEquals(numberOfMessages, assertionsPassed[0]);
+    }
+}
