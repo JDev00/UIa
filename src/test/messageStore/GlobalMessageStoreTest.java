153296c70ffe807000a6f5b88b21ca3faf301923
Manu <manuel00.dm@gmail.com>
2024-07-28 14:09:49 +0200

    updates tests

---
+++ src/test/messageStore/GlobalMessageStoreTest.java
@@ -0,0 +1,37 @@
+package messageStore;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import uia.application.message.store.ConcreteMessageStore;
+import uia.application.message.store.GlobalMessageStore;
+import uia.core.basement.message.MessageStore;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * GlobalMessageStore unit tests.
+ */
+
+class GlobalMessageStoreTest {
+    GlobalMessageStore globalMessageStore = GlobalMessageStore.getInstance();
+
+    @BeforeEach
+    public void beforeEach() {
+        MessageStore messageStore = new ConcreteMessageStore();
+        globalMessageStore.mount(messageStore);
+    }
+
+    @Test
+    void messageStoreShouldBeMountedAndUnmounted() {
+        // setup
+        MessageStore messageStoreToMount = new ConcreteMessageStore();
+
+        // act
+        globalMessageStore.mount(messageStoreToMount);
+        MessageStore unmountedStore = globalMessageStore.unmount();
+
+        // verify
+        assertEquals(messageStoreToMount, unmountedStore);
+    }
+}
